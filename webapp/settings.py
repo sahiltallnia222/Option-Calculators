eec271e7a85df3f04953c011a36e63c9b56561bc
SahilTallnia <sahiltallnia222@gmail.com>
2023-10-17 22:11:12 +0530

    upload

--- webapp/settings.py
+++ webapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webapp.urls'
@@ -118,7 +119,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT=BASE_DIR/'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT=BASE_DIR/'static'
 
 STATICFILES_DIRS=['webapp/static']
 
